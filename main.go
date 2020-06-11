a747426227dde9fb7ca4950b5a0e15804ce5ed4a
Takehiko Shinkura <tynmarket@gmail.com>
2020-06-11 22:43:10 +0900

    dynamodbに保存

--- main.go
+++ main.go
@@ -4,7 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"regexp"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/guregu/dynamo"
 )
 
 var reTitle = regexp.MustCompile("h2 data-attribute=\"(.+?)\"")
@@ -40,6 +46,18 @@ func crawl(url string) string {
 	return string(bytes)
 }
 
+type book struct {
+	Asin      string `dynamo:"asin"`
+	YearMonth string `dynamo:"year_month"`
+	Date      string `dynamo:"date"`
+	Title     string `dynamo:"title"`
+	Categoy   int    `dynamo:"category"`
+	CreatedAt string `dynamo:"created_at"`
+}
+
+var db *dynamo.DB
+var table dynamo.Table
+
 func parse(html string) {
 	titles := reTitle.FindAllStringSubmatch(html, -1)
 
@@ -54,5 +72,47 @@ func parse(html string) {
 
 		asin := reAsin.FindAllStringSubmatch(link[1], -1)
 		fmt.Printf("asin %d: %s\n", i, asin[0][1])
+
+		title := titles[i][1]
+		create(title, asin[0][1])
 	}
+}
+
+func create(title string, asin string) {
+	now := time.Now()
+	yearMonth := now.Format("2006-01")
+	date := now.Format("2006-01-02")
+	createdAt := now.Format(time.RFC3339)
+
+	record := book{
+		Asin:      asin,
+		YearMonth: yearMonth,
+		Date:      date,
+		Title:     title,
+		Categoy:   0,
+		CreatedAt: createdAt,
+	}
+
+	err := table.Put(record).Run()
+
+	if err == nil {
+		fmt.Printf("put book: %#v\n", record)
+	} else {
+		fmt.Printf("Failed to put item, err: %v\n", err)
+		fmt.Printf("book: %#v\n", record)
+	}
+}
+
+func init() {
+	dynamoDbLocal := os.Getenv("DYNAMO_DB_LOCAL")
+
+	if dynamoDbLocal == "true" {
+		db = dynamo.New(session.New(), &aws.Config{
+			Region:   aws.String("ap-northeast-1"),
+			Endpoint: aws.String("http://localhost:8000"),
+		})
+	} else {
+		dynamo.New(session.New(), &aws.Config{Region: aws.String("ap-northeast-1")})
+	}
+	table = db.Table("prime_books")
 }
